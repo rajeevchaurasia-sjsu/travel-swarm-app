80118d067756b665eee0144a8cd9093befd7e34b
Rajeev Chaurasia <rajeevchaurasia.dev@gmail.com>
2025-03-19 12:18:48 -0700

    Initialized TravelSwarm spring boot application

---
+++ travelSwarm/src/main/java/org/sjsu/travelswarm/TravelSwarmApplication.java
@@ -0,0 +1,13 @@
+package org.sjsu.travelswarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSwarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelSwarmApplication.class, args);
+    }
+
+}
